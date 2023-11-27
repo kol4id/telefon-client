70f70f8f03e34843b619502172379f81dcda3d5a
Kirill Nagulin <over90006at9@gmail.com>
2023-11-27 23:28:13 +0300

    added MainPane

---
+++ telefon/src/app/panes/MainPane.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+
+import styles from '../styles/MainPane.module.css'
+
+const MainPane: FC = () =>{ 
+    return(
+        <div className = {styles.main}>
+            
+        </div>
+    )
+}
+export default MainPane;

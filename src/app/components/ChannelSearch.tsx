932c59dd1139090dac5bcaec6bf68364ffaee4f9
Kirill Nagulin <over90006at9@gmail.com>
2024-01-29 19:19:36 +0300

    implemented channels search

--- src/app/components/ChannelSearch.tsx
+++ src/app/components/ChannelSearch.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { setSearchValue } from '../../store/states/channelSearch';
+import { SetSearchValue } from '../../store/states/channelSearch';
 
 import styles from '../styles/ChannelSearch.module.css'
 import { RootState } from '../../store/store';
 
 const ChannelSearch = () =>{
+
+    console.log("ChannelSearch rerender")
+
     const dispatch = useDispatch();
 
     const searchValue = useSelector((state:RootState) => state.channelSearch.value);
+    const width = useSelector((state: RootState) => state.width.searchWidth)
 
     const [focus, setFocus] = useState<boolean>(false);
     const [searchStyle, setSearchStyle] = useState<string>(styles.ChannelSearchLine)
@@ -24,11 +28,12 @@ const ChannelSearch = () =>{
             <input
                 onFocus={() => setFocus(true)}
                 onBlur={() => setFocus(false)}
-                onChange={(e) => dispatch(setSearchValue(e.target.value))}
+                onChange={(e) => dispatch(SetSearchValue(e.target.value))}
                 value={searchValue}
+                style={{width: width}}
             />
             <div className = {styles.inputClear}
-                onClick={() => dispatch(setSearchValue(''))}           
+                onClick={() => dispatch(SetSearchValue(''))}           
             ></div>
         </div>
     )

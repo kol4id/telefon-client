94f281a73001db2f8d895bbd6fc0901213ce936e
Kirill Nagulin <over90006at9@gmail.com>
2024-01-29 16:50:48 +0300

    MainPane cursor style and resize handler

--- src/app/panes/MainPane.tsx
+++ src/app/panes/MainPane.tsx
@@ -1,12 +1,50 @@
-import {FC} from 'react'
+import {FC, useEffect} from 'react'
+import AuthUser from '../api/authUser';
 
 import styles from '../styles/MainPane.module.css'
 import LeftPane from './LeftPane';
+import { useFethcing } from '../utils/hooks/useFetching';
+import MiddlePane from './MiddlePane';
+import React from 'react';
+import useLeftPaneResize from '../utils/hooks/useLeftPaneResize';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { SetCursorStyle } from '../../store/states/cursorStyle';
 
-const MainPane: FC = () =>{ 
+const MainPane: FC = () =>{
+    
+    // console.log("MainPane rerender")
+
+    const dispatch = useDispatch();
+    const cursorStyle = useSelector((state: RootState) => state.cursorStyle.value)
+
+    const {inResizePosition, currentlyResizing} = useLeftPaneResize();
+    const {fetching, isLoading} = useFethcing(async()=>{
+        await AuthUser();
+    })
+
+    useEffect(()=>{
+        fetching()
+    }, [])
+
+    useEffect(()=>{
+        if(inResizePosition || currentlyResizing){
+            dispatch(SetCursorStyle(true))
+        } else { dispatch(SetCursorStyle(false)) }
+    },[inResizePosition, currentlyResizing])
+
     return(
-        <div className = {styles.main}>
-            <LeftPane/>            
+        <div className = {styles.main}
+            style={{cursor: cursorStyle}}
+        >
+           {
+                isLoading
+                ?   <div></div>  
+                :   <React.Fragment>
+                        <LeftPane/>            
+                        <MiddlePane/>
+                    </React.Fragment> 
+           }
         </div>
     )
 }

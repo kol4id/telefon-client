2262f65cda1aded75eefc03b754e32e6a7e0c3a6
Kirill Nagulin <over90006at9@gmail.com>
2024-02-29 21:50:27 +0300

    Components| modalWrapper: basic html structure of modal window

---
+++ src/app/components/ModalWrapper.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+import { IPosition } from "../utils/interfaces/MousePos"
+import ReactDOM from "react-dom"
+
+import styles from '../styles/ModalWrapper.module.css'
+
+interface IProps{
+    isModalOpen: boolean,
+    modalPosition: IPosition,
+    modalContent: React.ReactNode
+}
+
+const ModalWrapper = (props: IProps) =>{
+
+    const [portalElement] = useState(document.getElementById('portal'))
+
+    const style: React.CSSProperties = {
+        top: props.modalPosition.y + 5,
+        left: props.modalPosition.x + 5,
+    }
+
+    return(
+        <React.Fragment>
+        {    
+            (props.isModalOpen && portalElement) &&
+                ReactDOM.createPortal(
+                    <div className={styles.modal_wrapper} style={style} onContextMenu={(e) => e.preventDefault()}>
+                        {props.modalContent}
+                    </div>, portalElement)        
+        }
+        </React.Fragment>
+    )
+}
+
+export default ModalWrapper

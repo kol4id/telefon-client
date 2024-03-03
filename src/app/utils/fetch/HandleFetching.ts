4d0d7b9df2498852714b648d3fcbcfae543c92bf
Kirill Nagulin <over90006at9@gmail.com>
2024-03-04 01:45:47 +0300

    API| new api call handler for non functional components

---
+++ src/app/utils/fetch/HandleFetching.ts
@@ -0,0 +1,23 @@
+import GetErrorMessage from "../errors/errorHandler";
+
+export const HandleFetching = (callback: () => Promise<any>): [fetching: ()=> Promise<any>, isLoading: boolean, error: {isObtained: boolean, message: string}] => {
+    let isLoading: boolean = false;
+    let error = {isObtained: false, message: ''};
+    let data: any = undefined;
+
+    const fetching = async(): Promise<any> => {
+        isLoading = true;
+        try {
+            data = await callback();
+        } catch(error: unknown){
+            const errorMessage = await GetErrorMessage(error);
+            error = {isObtained: true, message: errorMessage};
+        }finally {
+            isLoading = false;
+        }
+        return data;
+    }
+
+    return [fetching, isLoading, error];
+    // return {fetching, isLoading, error}
+}

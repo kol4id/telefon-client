ed5ad7150e8926cc770dd88e12ab73c35fef73b3
Kirill Nagulin <over90006at9@gmail.com>
2024-04-26 19:16:26 +0300

    test changes

--- src/app/components/Message.tsx
+++ src/app/components/Message.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from '../styles/Message.module.css';
 import { IMessage } from '../utils/interfaces/Message.dto';
+
+import send from '../../assets/send.png';
+import read from '../../assets/read.png';
+import { useAppDispatch } from '../../store/store';
+import { LastReadsQueuePush } from '../../store/states/messages';
+
 interface IProps{
     message: IMessage,
     self: boolean,
@@ -10,21 +16,52 @@ interface IProps{
 const Message = React.memo((props: IProps) =>{
 
     console.log(`message ${props.message.id} rerender`) 
+    const messageRef = useRef<HTMLDivElement>(null!);
+
+    const dispatch = useAppDispatch();
+
     const [messageStyle] = useState(
         props.self
         ? {default: styles.message_string_self, selected: styles.message_string_self_selected}
         : {default: styles.message_string, selected: styles.message_string_selected}
     );
 
+    const handleObserve = () => {
+        console.log(`message ${props.message.id} has been read`)
+        dispatch(LastReadsQueuePush(props.message));
+    }
+
+    const observer: IntersectionObserver = new IntersectionObserver(entries =>{
+        if(entries[0].isIntersecting){
+            handleObserve();
+            observer.unobserve(messageRef.current);
+        }
+    });
+
+    const startObserving = () =>{
+        if(props.message.isRead) return
+        observer.observe(messageRef.current);
+    }
+
+    useEffect(()=>{
+        startObserving();
+        return () => observer.disconnect();
+    }, [])
+     
+
     return(
         <div className = {props.selected ? messageStyle.selected : messageStyle.default }
             onContextMenu={(e) => {e.preventDefault()}}
+            ref={messageRef}
         >
             <div className = {styles.message_block}>
                 <div className = {styles.message_content}>
                     {props.message.content}
                     <div className={styles.send_time}>
                         {`${String(new Date(props.message.createdAt).getHours()).padStart(2, '0')}.${String(new Date(props.message.createdAt).getMinutes()).padStart(2, '0')}`}
+                        {
+                            props.self && <img className={styles.read_status} src={props.message.isRead ? read : send}></img>
+                        }
                     </div>
                 </div>
             </div>
--- src/app/components/MessagesList.tsx
+++ src/app/components/MessagesList.tsx
@@ -1,23 +1,38 @@
-import React, {useState} from "react"
+import React, { useCallback, useEffect, useRef, useState} from "react"
 import { useSelector } from "react-redux"
-import { RootState } from "../../store/store"
+import { RootState, useAppDispatch } from "../../store/store"
 import Message from "./Message"
 import ModalWrapper from "./ModalWrapper"
 import useModal from "../utils/hooks/useModal"
 import MessageListModalContent from "./MessageListModalContent"
+import InfiniteScroll from "./InfiniteScroll"
 
-const MessagesList = () =>{
+import styles from '../styles/MessageList.module.css'
+import { ClearLastReadQueue } from "../../store/states/messages"
+import { IMessage } from "../utils/interfaces/Message.dto"
+
+const MessagesList = React.memo(() =>{
 
     console.log("MessageList rerender")
 
+    const dispatch = useAppDispatch();
+
     const messageRecords = useSelector((state: RootState) => state.messages.messagesRecords);
     const currentChannelSelected = useSelector((state: RootState) => state.channelsList.currentChannelSelected);
-    const userId = useSelector((state: RootState) => state.user.userId);
+    const userId = useSelector((state: RootState) => state.user.userData.id);
+    // const lastReadsQueue = useSelector((state: RootState) => state.messages.lastReadsQueue);
+
+    // const [lastReadQ, setLastReadQ] = useState<IMessage[]>([]);
     
     const [selected, setSelected] = useState<string>('');
     const [isModalOpen, modalPosition, modalContent, openModal, closeModal] = useModal();
 
-    const selectMessage = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, id: string, index: number): void  =>{   
+    // const handleObserve = useCallback((message: IMessage) =>{
+    //     console.log(`message ${message.id} has been read`)
+    //     setLastReadQ([...lastReadQ, message]);
+    // }, [])
+
+    const SelectMessage = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, id: string, index: number): void  =>{   
         event.preventDefault();
         if(selected === id){
             setSelected('');
@@ -31,21 +46,47 @@ const MessagesList = () =>{
         }     
     }
 
+    const firstElement = () => {
+        console.log('first')
+    }
+
+    const lastElement = () => {
+        console.log('last')
+    }
+
+    useEffect(()=>{
+        closeModal()
+    }, [currentChannelSelected])
+
+    // useEffect(()=>{
+    //     let intervalId: number;
+    //     if(lastReadsQueue.length){
+    //         intervalId = setInterval(()=>{
+    //             console.log('asdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
+    //             setLastReadQ([]);
+    //             // dispatch(ClearLastReadQueue());
+    //         }, 500)
+    //     }
+    //     return () => clearInterval(intervalId);
+    // }, [lastReadsQueue])
+
     return(
-        <React.Fragment>
-        {
-            messageRecords[currentChannelSelected]
-            ?   messageRecords[currentChannelSelected].map((message, index) => 
-                    <div onContextMenu={(event) => selectMessage(event, message.id, index)}>
+        <div id="transition" className={styles.message_list_main}>
+            <InfiniteScroll callback={lastElement}/>
+            {
+                messageRecords[currentChannelSelected].map((message, index)=>
+                    <div onContextMenu={(event) => SelectMessage(event, message.id, index)}>
                         <Message message={message} self={message.creatorId === userId} selected={selected === message.id} key={message.id}/>
-                    </div>  
+                    </div>                               
                 )
-            :   <div></div>
-        }
+            } 
+            <InfiniteScroll callback={firstElement}/>
         {
             <ModalWrapper isModalOpen={isModalOpen} modalPosition={modalPosition} modalContent={modalContent}/>   
         }
-        </React.Fragment>
+        </div>
     )
-}
+})
 export default MessagesList
+
+

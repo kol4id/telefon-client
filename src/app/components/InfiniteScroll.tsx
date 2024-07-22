fe1b0be7ed983e64f85cfe2e49b63276fd1ec860
Kirill Nagulin <over90006at9@gmail.com>
2024-07-22 19:56:48 +0300

    infinite scroll now handle scroll up/down differently

--- src/app/components/InfiniteScroll.tsx
+++ src/app/components/InfiniteScroll.tsx
@@ -1,31 +1,50 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface IProps {
     callback: ()=> void;
+    // direction: "top" | 'bottom';
     // children: React.ReactNode;
 }
 
-const InfiniteScroll: React.FC<IProps> = React.memo(({callback}) =>{
+const InfiniteScroll: React.FC<IProps> = ({callback}) =>{
     const infiniteRef = useRef(null!);
+    const [previousY, setPreviousY] = useState<number>(0);
+    const [previousRatio, setPreviousRatio] = useState<number>(0);
 
     useEffect(()=>{
-        const observer = new IntersectionObserver( entries =>{
-            callback();
-            if(entries[0].isIntersecting){
-                // observer.unobserve(infiniteRef.current)
-            }
+        const observer = new IntersectionObserver(entries =>{
+            entries.forEach(entry => {
+                const currentY = entry.boundingClientRect.y;
+                const currentRatio = entry.intersectionRatio;
+                const isIntersecting = entry.isIntersecting;
+
+                if (currentY > previousY && isIntersecting){
+                    if (currentRatio > previousRatio){
+                        callback();
+                    }
+                } else if (currentY < previousY && isIntersecting) {
+                    if (currentRatio > previousRatio){
+                        callback();
+                    }
+                }
+                setPreviousRatio(currentRatio);
+                setPreviousY(currentY);
+            })
         }, {
             root: document.getElementById('bodyTransition'),
             rootMargin: '200px'
         })
+        // document.getElementById('bodyTransition')
 
         observer.observe(infiniteRef.current);
+        
         return () => observer.disconnect();
-    })
+    },[])
 
     return(
-        <div ref={infiniteRef}/>
+        <div ref={infiniteRef}
+        style={{border: "1px solid red"}}/>
     )
-})
+}
 
 export default InfiniteScroll;

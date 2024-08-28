af8152edb43518a597610858d85b571cbf9ae3ae
Kirill Nagulin <over90006at9@gmail.com>
2024-08-28 17:17:27 +0300

    message manager no longer handle InfScroll

--- src/app/components/MessagesManager.tsx
+++ src/app/components/MessagesManager.tsx
@@ -1,32 +1,19 @@
-import React, { useCallback, useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
-import { RootState, useAppDispatch, } from "../../store/store"
-import Message from "./Message"
+import { RootState } from "../../store/store"
 import ModalWrapper from "./ModalWrapper"
 import useModal from "../utils/hooks/useModal"
 import MessageListModalContent from "./MessageListModalContent"
-import InfiniteScroll from "./InfiniteScroll"
 
-import styles from '../styles/MessageList.module.css'
 import MessageBroker from "./MessageBroker"
-import { messagePushMessage, messageShiftMessage } from "../../store/states/messages"
-import { IMessage } from "../global/types/Message.dto"
-import { IFetchMessages } from "../api/api"
 import MessagesList from "./MessagesList"
 import EmptyMessageList from "./EmptyMessageList"
 
-// const api = new API(baseUrl)
 
 const MessagesManager = React.memo(() =>{
-
     console.log("MessageList rerender")
-    
-    const dispatch = useAppDispatch();
-
     const messageRecords = useSelector((state: RootState) => state.messages.messagesRecords);
     const currentChannelSelected = useSelector((state: RootState) => state.channelsList.currentChannelSelected);
-    const user = useSelector((state: RootState) => state.user.userData);
-    const currentMessageRecordsRef = useRef(messageRecords[currentChannelSelected]);
     
     const [selected, setSelected] = useState<string>('');
     const [isModalOpen, modalPosition, modalContent, openModal, closeModal] = useModal();
@@ -45,56 +32,19 @@ const MessagesManager = React.memo(() =>{
         }     
     }
 
-    // const fetchMessages = async(args: IFetchMessages, dispatch: any): Promise<void>=>{
-    //     // const messages = await api.fetchMessages(args);
-    //     // dispatch(messages);
-    // }
-
-    const firstElement = useCallback(() => {
-        console.log('first')
-        const lastMessageIndx = currentMessageRecordsRef.current.length - 1;
-                    // fetchMessages({
-                    //     channelId: currentChannelSelected,
-                    //     limit: 25, 
-                    //     endDate: currentMessageRecordsRef.current[lastMessageIndx].createdAt
-                    // }, (msg: IMessage[])=>{dispatch(messagePushMessage(msg))})
-        // dispatch(fetchMessages({
-        //     channelId: currentChannelSelected,
-        //     limit: 25, 
-        //     endDate: messageRecords[currentChannelSelected][0].createdAt
-        // }));
-    }, [currentChannelSelected, messageRecords[currentChannelSelected]])
-
-    const lastElement = useCallback(() => {
-        console.log(`last`)
-        const messageDate = currentMessageRecordsRef.current[0].createdAt;
-        // const lastMessageIndx = messageRecords[currentChannelSelected].length - 1;
-        // dispatch(fetchMessages({
-        //     channelId: currentChannelSelected,
-        //     limit: 25, 
-        //     startDate: messageRecords[currentChannelSelected][lastMessageIndx].createdAt
-        // }));
-                    // fetchMessages({
-                    //     channelId: currentChannelSelected,
-                    //     limit: 25, 
-                    //     startDate: messageDate
-                    // }, (msg: IMessage[])=>{dispatch(messageShiftMessage(msg))})
-    }, [currentMessageRecordsRef.current, messageRecords[currentChannelSelected], currentChannelSelected])
-
     useEffect(()=>{
         closeModal()
     }, [currentChannelSelected])
 
     return(
-        <div id="transition" style={{display: "flex"}}>
+        <div id="message_transition" style={{display: "flex"}}>
             {
                 messageRecords?.[currentChannelSelected]
                 ?   <>
-                        <InfiniteScroll callback={lastElement}/>
-                            <MessageBroker>
-                                <MessagesList selected={selected} callback={SelectMessage}/>
-                            </MessageBroker>
-                        <InfiniteScroll callback={firstElement}/>
+                        <MessageBroker>
+                            <MessagesList selected={selected} callback={SelectMessage}/>
+                        </MessageBroker>
+                
                     </>
 
                 :   <EmptyMessageList/>

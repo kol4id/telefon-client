f3319fa2cfc26f409b765ba7f754f9fe99b3a2a4
Kirill Nagulin <over90006at9@gmail.com>
2024-08-28 17:19:24 +0300

    now handling infScroll

--- src/app/components/MessagesList.tsx
+++ src/app/components/MessagesList.tsx
@@ -1,30 +1,70 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
-import { FC, memo } from "react";
+import { RootState, useAppDispatch } from "../../store/store";
+import { FC, memo, useCallback, useMemo, useRef } from "react";
 import Message from "./Message";
-import EmptyMessageList from "./EmptyMessageList";
-import styles from '../styles/MessageList.module.css'
+import styles from '../styles/MessageList.module.css';
+import InfiniteScroll from "./InfiniteScroll";
+import { fetchMessages } from "../../store/states/messages";
+import { IFetchMessages } from "../api/api";
+
 interface IProps{
     callback: (p1: any, p2: any, p3: any)=>void;
     selected: string
 }
 
 const MessagesList: FC<IProps> = memo((props) => {
+    const dispatch = useAppDispatch();
     const messageRecords = useSelector((state: RootState) => state.messages.messagesRecords);
     const currentChannelSelected = useSelector((state: RootState) => state.channelsList.currentChannelSelected);
     const user = useSelector((state: RootState) => state.user.userData);
 
+    // const [messageList] = useState<IMessage[]>(messageRecords[currentChannelSelected]);
+    const messageList = useMemo(() => messageRecords[currentChannelSelected], [messageRecords, currentChannelSelected]);
+    const messageRecordsRef = useRef(messageRecords);
+    const currentChannelSelectedRef = useRef(currentChannelSelected);
+    const messageListRef = useRef(messageList);
+
+    messageRecordsRef.current = messageRecords;
+    currentChannelSelectedRef.current = currentChannelSelected;
+    messageListRef.current = messageList;
+    
+    const defaultOptions: IFetchMessages = {
+        channelId: currentChannelSelected, 
+        limit: 25
+    }
+
+    //NOTE(@kol4id): automaticly add 1ms to start date and remove 1ms to end date
+    const fetchMessagesIScroll = async(startD?: Date, endD?: Date) =>{
+        const startDate = startD ? new Date(new Date(startD).getTime() + 1) : undefined;
+        const endDate = endD ? new Date(new Date(endD).getTime() - 1) : undefined;
+        await dispatch(fetchMessages({
+            ...defaultOptions,
+            startDate,
+            endDate 
+        }))
+    }
+
+    const handleFetchMessagesBottom = useCallback(() => {
+        fetchMessagesIScroll(messageListRef.current?.[0]?.createdAt);
+    }, []);
+
+    const handleFetchMessagesTop = useCallback(() => {
+        fetchMessagesIScroll(undefined, messageListRef.current?.[messageListRef.current.length - 1]?.createdAt);
+    }, []);
+
+    //NOTE(@kol4id): inf scroll reversed because list is reversed
     return(
-        <section className={styles.message_list_main}>
-        {
-            messageRecords[currentChannelSelected]?.map((message, index)=>
-                <div onContextMenu={(event) => props.callback(event, message.id, index)} key={message.id}>
-                    <Message message={message} self={message.creatorId === user.id} selected={props.selected === message.id} />
-                </div>                               
-            )
-        }
+        <section id="message_list_main" className={styles.message_list_main}>
+            <InfiniteScroll direction="bottom" callback={handleFetchMessagesBottom}/> 
+            {
+                messageList?.map((message, index)=>
+                    <div onContextMenu={(event) => props.callback(event, message.id, index)} key={message.id}>
+                        <Message message={message} self={message.creatorId === user.id} selected={props.selected === message.id} />
+                    </div>                               
+                )
+            }
+            <InfiniteScroll direction="top" callback={handleFetchMessagesTop}/>
         </section>
     )
 })
-
 export default MessagesList

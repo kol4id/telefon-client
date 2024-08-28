d8ae88f74f601b8637d2df755f61da9dd68b8486
Kirill Nagulin <over90006at9@gmail.com>
2024-08-28 17:18:45 +0300

    now using absolute object with pading instead of rootMargin

--- src/app/components/InfiniteScroll.tsx
+++ src/app/components/InfiniteScroll.tsx
@@ -1,49 +1,53 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 interface IProps {
     callback: ()=> void;
-    // direction: "top" | 'bottom';
+    direction: "top" | 'bottom';
     // children: React.ReactNode;
 }
 
-const InfiniteScroll: React.FC<IProps> = ({callback}) =>{
+const threshold = [0.1,0.9];
+
+const InfiniteScroll: React.FC<IProps> = ({callback, direction}) =>{
     const infiniteRef = useRef(null!);
-    const [previousY, setPreviousY] = useState<number>(0);
-    const [previousRatio, setPreviousRatio] = useState<number>(0);
+    const previousYRef = useRef<number>(0);
+    const previousRatioRef = useRef<number>(0);
+
+    const handleObserve = (entries: IntersectionObserverEntry[]) => {
+        entries.forEach(entry => {
+            const currentY = entry.boundingClientRect.y;
+            const currentRatio = entry.intersectionRatio;
+            const isIntersecting = entry.isIntersecting;
+            
+            if (currentY < previousYRef.current && isIntersecting){
+                if (currentRatio > previousRatioRef.current){
+                    callback();
+                }
+            } else if (currentY > previousYRef.current && isIntersecting) {
+                if (currentRatio > previousRatioRef.current){
+                    callback();
+                }
+            }
+
+            previousYRef.current = currentY;
+            previousRatioRef.current = currentRatio;
+        })
+    }
 
     useEffect(()=>{
         const observer = new IntersectionObserver(entries =>{
-            entries.forEach(entry => {
-                const currentY = entry.boundingClientRect.y;
-                const currentRatio = entry.intersectionRatio;
-                const isIntersecting = entry.isIntersecting;
-
-                if (currentY > previousY && isIntersecting){
-                    if (currentRatio > previousRatio){
-                        callback();
-                    }
-                } else if (currentY < previousY && isIntersecting) {
-                    if (currentRatio > previousRatio){
-                        callback();
-                    }
-                }
-                setPreviousRatio(currentRatio);
-                setPreviousY(currentY);
-            })
+            handleObserve(entries)
         }, {
-            root: document.getElementById('bodyTransition'),
-            rootMargin: '200px'
-        })
-        // document.getElementById('bodyTransition')
-
+            threshold: threshold
+        })  
+    
         observer.observe(infiniteRef.current);
-        
         return () => observer.disconnect();
     },[])
 
     return(
         <div ref={infiniteRef}
-        style={{border: "1px solid red"}}/>
+        style={{ top: direction == "top" ? "0px" : "", padding: "70px", position: "absolute", border: "1px solid red"}}/>
     )
 }
 

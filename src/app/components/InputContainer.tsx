8fb5a02bf1925af3c29a5fe0bed6e1806b5453ea
Kirill Nagulin <over90006at9@gmail.com>
2024-10-15 23:25:49 +0300

    decreased js now all textarea resizes by css

--- src/app/components/InputContainer.tsx
+++ src/app/components/InputContainer.tsx
@@ -4,61 +4,47 @@ import { SetMessageInput } from '../../store/states/messageInput';
 import { RootState, useAppDispatch } from '../../store/store';
 
 import styles from '../styles/InputContainer.module.css';
-import { socketCreateMessage} from '../../store/states/socket';
-import { IChannel } from '../global/types/Channel.dto';
-import { subscribeToChannel } from '../../store/states/channels';
+import { socketCreateMessage } from '../../store/states/socket';
 
 
 const InputContainer = () =>{
-
     console.log("InputContainer rerender")
 
     const dispatch = useAppDispatch();
     const messageInputValue = useSelector((state:RootState) => state.messageInput.value);
-    const sendMessageMargin = useSelector((state:RootState) => state.messageInput.height);
     const currentChannelSelected = useSelector((state:RootState) => state.channelsList.currentChannelSelected);
-    const userChannels = useSelector((state: RootState) => state.channelsList.userChannels);
-
-    const isSubscribed = (channels: IChannel[]): Boolean => {
-        const indx = channels.findIndex(channel => channel.id == currentChannelSelected);
-        if (indx < 0) return false
-        return true
-    }
-
-    const sendMessage = async() =>{
-        if (!isSubscribed(userChannels)){
-            await dispatch(subscribeToChannel(currentChannelSelected))
-        }
-
-        dispatch(socketCreateMessage({
-            channelId: currentChannelSelected, 
-            content: messageInputValue, 
-            hasMadia: false
-        }))
-        dispatch(SetMessageInput(''))
-    }
 
     const sendMessageOnEnter = async(event: any) => {
-        if (!messageInputValue) return;
         if (event.key === "Enter" && !event.ctrlKey && !event.altKey && !event.shiftKey){
             sendMessage();
         }
     }
 
+    const sendMessage = async() =>{
+        if (!messageInputValue) return;
+
+        dispatch(socketCreateMessage({
+            channelId: currentChannelSelected, 
+            content: messageInputValue, 
+            hasMedia: false
+        }))
+        dispatch(SetMessageInput(''))
+    }
+
     return(
-        <div className = {styles.inputContainer}
+        <footer className = {styles.input_area}
             onKeyDown={(event) => sendMessageOnEnter(event)}
         >
-            <div className = {styles.inputBox}>
-                <MessageInput/>
-            </div>
-
-            <div className = {styles.sendMessageButton}
-                onClick={() => sendMessage()}
-                style={{marginTop: sendMessageMargin - 20}}  
-            >
-            </div>
-        </div>
+            <section className = {styles.input_container}>
+                <div className = {styles.input_box}>
+                    <MessageInput/>
+                </div>
+                <button className = {styles.send_message_button}
+                    onClick={() => sendMessage()}
+                >
+                </button>
+            </section>
+        </footer>
     )
 }
 export default InputContainer;
--- src/app/components/MessageInput.tsx
+++ src/app/components/MessageInput.tsx
@@ -3,21 +3,10 @@ import styles from '../styles/MessageInput.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../store/store'
 import { SetMessageInput, SetMessageInputHeight } from '../../store/states/messageInput'
-import useWindowSize from '../utils/hooks/useWindowSize'
-import { useEffect, useState } from 'react'
 
 const MessageInput = () =>{
-
-    console.log("MessageInput rerender")
-
     const dispatch = useDispatch();
-    
-    const ROW_SIZE = 20;
-
-    const innerWindowHeight = useWindowSize().height;
     const inputValue = useSelector((state:RootState) => state.messageInput.value)
-    const [maxHeight, setMaxHeight] = useState(innerWindowHeight * 0.25);
-    const [maxRows, setMaxRows] = useState<number>(maxHeight / ROW_SIZE);
   
     const preventEnter = (event: any) =>{
         if (event.key === 'Enter' && !event.ctrlKey && !event.altKey && !event.shiftKey){
@@ -25,19 +14,14 @@ const MessageInput = () =>{
         }
     }
 
-    useEffect(()=>{
-        setMaxHeight(innerWindowHeight * 0.25);
-        setMaxRows(maxHeight / ROW_SIZE)
-    }, [innerWindowHeight])
-
     return(
         <TextareaAutosize className={styles.textarea}
             onKeyDown = {(event) => {preventEnter(event)}}
             onChange={(e) => dispatch(SetMessageInput(e.target.value))}
             onHeightChange = {(height) => dispatch(SetMessageInputHeight(height))}
             value={inputValue}
-            minRows = {1}
-            maxRows = {maxRows}
+            minRows = {1}   
+            maxLength={3000}
         />
     )
 }

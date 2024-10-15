2b9e576cce96b30dce1988a99e6f0c5543615407
Kirill Nagulin <over90006at9@gmail.com>
2024-10-15 20:17:01 +0300

    packages added, new ts version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'store': path.resolve(__dirname, 'src/store')
+    }
+  }
+});
+

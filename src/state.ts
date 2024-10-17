d66f31f86db8015906786beb45be4c985fd08ff0
Kirill Nagulin <over90006at9@gmail.com>
2024-10-17 22:48:24 +0300

    new server api

--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,9 @@
-const serverProtocol = `http`
-const serverAddress = `51.250.38.46` //localhost
+const serverProtocol = `https`
+// const serverAddress = `server-telefon.duckdns.org/` //localhost
+const subDomain = `server-telefon`
+const domain = `duckdns`
+const topDomain = `org`
 const serverPort = `4200`
 const serverEntry = `api`
 // export const baseUrl = `${serverProtocol}://${serverAddress}:${serverPort}/${serverEntry}`;
-export const baseUrl = `${serverProtocol}://${serverAddress}:${serverPort}/${serverEntry}`
+export const baseUrl = `${serverProtocol}://${subDomain}.${domain}.${topDomain}/${serverEntry}`

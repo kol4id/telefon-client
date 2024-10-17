3c3b75da53e15620fb834a99816e47db78d3bff9
Kirill Nagulin <over90006at9@gmail.com>
2024-10-17 20:28:48 +0300

    server addres changed

--- src/state.ts
+++ src/state.ts
@@ -1,5 +1,6 @@
 const serverProtocol = `http`
-const serverAddress = `localhost`
+const serverAddress = `51.250.38.46` //localhost
 const serverPort = `4200`
 const serverEntry = `api`
-export const baseUrl = `${serverProtocol}://${serverAddress}:${serverPort}/${serverEntry}`;
+// export const baseUrl = `${serverProtocol}://${serverAddress}:${serverPort}/${serverEntry}`;
+export const baseUrl = `${serverProtocol}://${serverAddress}:${serverPort}/${serverEntry}`
